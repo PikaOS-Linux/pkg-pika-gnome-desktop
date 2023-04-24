c4978dffdceb88f0c8e242c03e09c0b09a9abb28
ferrreo <harderthanfire@gmail.com>
2023-04-24 17:44:58 +0100

    Update to lunar

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ dpkg-sig --sign builder ./output/pika-gnome-desktop*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-gnome-desktop*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-gnome-desktop*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-gnome-desktop*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-gnome-desktop*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
